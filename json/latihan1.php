74df15d5ca09657fa6d5c43bf8f1b6bbf716f33b
eganovianingsih <eganovia07@mail.unpas.ac.id>
2019-12-04 17:14:45 +0700

    menambahkan file coba.json dan latihan1

---
+++ json/latihan1.php
@@ -0,0 +1,29 @@
+	<?php 
+
+	// $mahasiswa = [
+	// 	[
+	// 		"nama" => "ega novianingsih",
+	// 		"nrp" => " 173040036",
+	// 		"email" => "eganovia07@mail.unpas.ac.id"
+			
+	// 	],
+
+	// 	[
+	// 		"nama" => "reza",
+	// 		"nrp" => " 173040113",
+	// 		"email" => "zapram123@mail.unpas.ac.id"
+		
+	// 	]
+	// ];
+
+	$dbh = new PDO('
+		mysql:host=localhost;dbname=phpmvc','root',
+		'');
+	$db = $dbh->prepare('SELECT * FROM mahasiswa');
+	$db->execute();
+	$mahasiswa = $db->fetchAll(PDO::FETCH_ASSOC);
+
+	$data = json_encode($mahasiswa);
+	echo $data;
+
+ ?>
