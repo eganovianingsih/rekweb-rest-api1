1411d8ee30953d26bcc6923fe307ec6b67da284f
eganovianingsih <eganovia07@mail.unpas.ac.id>
2019-12-04 17:33:17 +0700

    memperbaiki yang error baris 21

--- json/latihan1.php
+++ json/latihan1.php
@@ -16,14 +16,13 @@
 	// 	]
 	// ];
 
-	$dbh = new PDO('
-		mysql:host=localhost;dbname=phpmvc','root',
-		'');
+	$dbh = new PDO('mysql:host=localhost;dbname=phpmvc','root','');
 	$db = $dbh->prepare('SELECT * FROM mahasiswa');
 	$db->execute();
 	$mahasiswa = $db->fetchAll(PDO::FETCH_ASSOC);
 
 	$data = json_encode($mahasiswa);
 	echo $data;
+
 
  ?>
